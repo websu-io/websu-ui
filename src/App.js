a2f237df4163dbb2d38e64a19d5f0f8cf706b5b7
Sam Stoelinga <sammiestoel@gmail.com>
2020-08-01 22:59:58 -0700

    Render lighthouse json file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReportViewer from 'react-lighthouse-viewer';
+
+import jsonReport from './report.json';
+
 
 function App() {
   return (
@@ -10,15 +14,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <ReportViewer json={jsonReport} />
     </div>
   );
 }
