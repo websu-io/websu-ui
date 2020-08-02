8f568a2750a8d01c9650747ccdb807ccf4689cb6
Sam Stoelinga <sammiestoel@gmail.com>
2020-08-02 00:06:57 -0700

    Add bootstrap and scanbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ReportViewer from 'react-lighthouse-viewer';
 
@@ -9,15 +8,24 @@ import jsonReport from './report.json';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <ScanBar/>
       <ReportViewer json={jsonReport} />
     </div>
   );
 }
+
+
+class ScanBar extends React.Component {
+    render() {
+        return (
+<form class="form-inline mr-auto mb-4">
+          <input className="form-control mr-sm-2" type="text" placeholder="URL..." aria-label="Scan"></input>
+          <button className="btn btn-outline-primary btn-rounded btn-sm my-0 waves-effect waves-light" type="submit">Scan</button>
+        </form>
+        )
+    }
+}
+
+
 
 export default App;
